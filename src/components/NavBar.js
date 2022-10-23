0154a2ca8b02e6430f48bdfc764710e81bd8360a
arghya <princearghya824@gmail.com>
2022-10-23 19:38:05 +0530

    event commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -52,6 +52,27 @@ function NavBar() {
   )
 }
 
+const menuBtn = document.querySelector('.menu-btn');
+      const navbar = document.querySelector('.navbar ul');
+        let menuOpen = false;
+        if(menuBtn){
+        menuBtn.addEventListener('click',()=>{
+          if(!menuOpen) {
+            menuBtn.classList.add('open');
+            navbar.classList.add('open');
+            menuOpen = true;
+          } else {
+            menuBtn.classList.remove('open');
+            navbar.classList.remove('open');
+            menuOpen = false;
+          }
+          document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click",()=>{
+            menuBtn.classList.remove('open');
+            navbar.classList.remove('open');
+            menuOpen = false;
+          }))
+        })};
+
 
 
 export default NavBar
